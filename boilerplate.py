f91883b4579bcb1eea36525d4eafef0fcd922a70
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2022-12-13 11:15:08 +0100

    remove YEAR, DATE from solution scripts
    aoc determines year and date from file path

--- boilerplate.py
+++ boilerplate.py
@@ -16,14 +16,12 @@ def main_b(data):
 if __name__ == "__main__":
     from sys import argv, stdin
 
-    YEAR, DAY = 2021, 1
-
     PART = argv[1] if len(argv) > 1 else "a"
     if not stdin.isatty():
         data = stdin.read()
     else:
-        from aocd import get_data
-
-        data = get_data(day=DAY, year=YEAR)
+        # because year and day can be found in path of this file
+        # aocd can determine which data to pull automatically.
+        from aocd import data
 
     main(data) if PART == "a" else main_b(data)

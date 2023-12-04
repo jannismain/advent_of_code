e841d08484885057d91809b5a61737ed7c5472ec
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-12-04 12:29:09 +0100

    cleanup 2

--- 2023/02.py
+++ 2023/02.py
@@ -4,6 +4,7 @@ from math import prod
 
 CUBES_AVAILABLE = {"red": 12, "green": 13, "blue": 14}
 
+
 def main(data, part="a"):
     result = 0
     for game_record in data.splitlines():
@@ -21,41 +22,49 @@ def main(data, part="a"):
                     break
             else:
                 set_ok = True
-            if not set_ok:
-                if part == "a":
-                    print("❌")
-                    break
+            if not set_ok and part == "a":
+                print("❌")
+                break
         else:
-            if part=="a":
+            if part == "a":
                 print("✅")
             else:
                 print(cubes_found)
 
             if part == "a":
                 result += game_id
-        if part=="b":
+        if part == "b":
             result += prod(cubes_found.values())
     print(result)
     return result
 
-def CubesExceeded(Exception):
-    pass
 
 def main_b(data):
     return main(data, part="b")
 
 
 def test():
-    assert main("""Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    assert (
+        main(
+            """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""") == 8
-    assert main("""Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+        )
+        == 8
+    )
+    assert (
+        main(
+            """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""", part="b") == 2286
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""",
+            part="b",
+        )
+        == 2286
+    )
 
 
 if __name__ == "__main__":

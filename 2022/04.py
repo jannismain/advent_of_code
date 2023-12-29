d602fdf84610785d0771d1fbb80921c20c196ea8
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-12-29 10:38:00 +0100

    fix: pytest test discovery
    wrap 04/22 in main clause

--- 2022/04.py
+++ 2022/04.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
-from sys import argv
 
-PART = argv[1] if len(argv) > 1 else "a"
+if __name__ == "__main__":
+    from sys import argv
 
-from sys import stdin
+    PART = argv[1] if len(argv) > 1 else "a"
 
-count = 0
-for assignment in stdin:
-    first, second = [[int(i) for i in elf.split("-")] for elf in assignment.split(",")]
-    if PART == "a":  # only count fully overlapping assignments
-        if (
-            first[0] <= second[0]
-            and first[1] >= second[1]
-            or first[0] >= second[0]
-            and first[1] <= second[1]
-        ):
-            count += 1
-    elif PART == "b":  # count all overlapping assignments
-        # de-morgan of first[1] < second[0] or first[0] > second[1]
-        if first[1] >= second[0] and first[0] <= second[1]:
-            count += 1
-print(count)
+    from sys import stdin
+
+    count = 0
+    for assignment in stdin:
+        first, second = [[int(i) for i in elf.split("-")] for elf in assignment.split(",")]
+        if PART == "a":  # only count fully overlapping assignments
+            if (
+                first[0] <= second[0]
+                and first[1] >= second[1]
+                or first[0] >= second[0]
+                and first[1] <= second[1]
+            ):
+                count += 1
+        elif PART == "b":  # count all overlapping assignments
+            # de-morgan of first[1] < second[0] or first[0] > second[1]
+            if first[1] >= second[0] and first[0] <= second[1]:
+                count += 1
+    print(count)
